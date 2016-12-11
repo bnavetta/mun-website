34328d03a2cba6c0b4b28023efc3ba8d7fba6e7e
Ben Navetta <ben.navetta@gmail.com>
2016-12-11 12:15:39 -0500

    all the things for adam

---
+++ core/src/main/java/org/brownmun/web/admin/NoFreeDelegatesException.java
@@ -0,0 +1,8 @@
+package org.brownmun.web.admin;
+
+/**
+ * Created by ben on 12/5/16.
+ */
+public class NoFreeDelegatesException
+{
+}
--- frontend/src/admin/committee/index.jsx
+++ frontend/src/admin/committee/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import './positions-page.scss';
+
 function renderAddPositions(elem) {
 	System.import('./add-positions').then(module => {
 		const AddPositions = module.default;
