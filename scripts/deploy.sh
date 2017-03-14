d2a953b9d4eb4501c4c67d6431d71c7f09c8ce03
Ben Navetta <ben.navetta@gmail.com>
2017-03-14 14:16:14 -0400

    fix more things

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,5 +4,5 @@ BUSUN_JAR="busun/build/libs/busun-$TRAVIS_BUILD_NUMBER.jar"
 
 gcloud compute copy-files $BUSUN_JAR brownmun:/srv/busun/busun-$TRAVIS_BUILD_NUMBER.jar
 gcloud compute ssh brownmun --command="ln -s /srv/busun/busun-$TRAVIS_BUILD_NUMBER.jar /srv/busun/busun-latest.jar"
-gcloud compute ssh brownmun --commmand="chown /srv/busun/busun-*.jar busun:busun"
+gcloud compute ssh brownmun --command="chown busun:busun /srv/busun/busun-*.jar"
 #gcloud compute ssh busun@brownmun --command="sudo mv /tmp/busun.jar /srv/busun-$TRAVIS_BUILD_NUMBER.jar"
