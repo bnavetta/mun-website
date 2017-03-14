1e906d1aaca9e77cd3f290682b44d02e143a4797
Ben Navetta <ben.navetta@gmail.com>
2017-03-14 14:41:05 -0400

    no sudo

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,4 +3,4 @@
 BUSUN_JAR="busun/build/libs/busun-$TRAVIS_BUILD_NUMBER.jar"
 
 gcloud compute copy-files $BUSUN_JAR brownmun:/srv/busun/busun-$TRAVIS_BUILD_NUMBER.jar
-gcloud compute ssh brownmun --command="sudo /srv/busun/activate_version.sh $TRAVIS_BUILD_NUMBER"
+gcloud compute ssh brownmun --command="/srv/busun/activate_version.sh $TRAVIS_BUILD_NUMBER"
