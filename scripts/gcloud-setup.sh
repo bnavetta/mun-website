c1aaf29e4a2ba0ccc78fd4cd83f109732dfbe4c5
Ben Navetta <ben.navetta@gmail.com>
2017-03-14 13:34:01 -0400

    travis why

--- scripts/gcloud-setup.sh
+++ scripts/gcloud-setup.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
+echo "PATH=$PATH"
+
 if [ ! -d ${HOME}/google-cloud-sdk ]; then
     echo "Installing gcloud sdk"
     curl https://sdk.cloud.google.com | bash
+else
+    echo "gcloud sdk already installed"
 fi
+
+ls -l ${HOME}/google-cloud-sdk/bin
 
 gcloud auth activate-service-account --key-file client-secret.json
 gcloud config set project busun-158105
