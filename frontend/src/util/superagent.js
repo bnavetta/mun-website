d1f7b86c0addaa1e1a77b481b7515e67cdaa60d0
Ben Navetta <ben.navetta@gmail.com>
2017-03-16 23:45:38 -0400

    move csrf token out of inline js

--- frontend/src/util/superagent.js
+++ frontend/src/util/superagent.js
@@ -5,10 +5,11 @@ import superagentUse from 'superagent-use';
 
 const agent = superagentUse(superagent);
 
+const csrfHeader = document.querySelector('meta[name=csrf-header]').getAttribute('content');
+const csrfToken = document.querySelector('meta[name=csrf-token]').getAttribute('content');
+
 const csrf = (request) => {
-    if (window.csrf) {
-        request.set(window.csrf.header, window.csrf.token);
-    }
+    request.set(csrfHeader, csrfToken);
 };
 
 agent.use(csrf);
