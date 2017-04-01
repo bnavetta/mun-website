bd83c705a7d2a8b37e077c8825030d0478230dec
Ben Navetta <ben.navetta@gmail.com>
2017-04-01 11:17:00 -0400

    don't directly overwrite jar - permissions issue

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,6 +2,5 @@
 
 BUSUN_JAR="busun/build/libs/busun-$TRAVIS_BUILD_NUMBER.jar"
 
-gcloud compute ssh brownmun --command="/srv/busun/predeploy.sh"
-gcloud compute copy-files $BUSUN_JAR brownmun:/srv/busun/busun.jar
-gcloud compute ssh brownmun --command="/srv/busun/redeploy.sh"
+gcloud compute copy-files $BUSUN_JAR brownmun:/srv/busun/busun-new.jar
+gcloud compute ssh brownmun --command="/srv/busun/deploy.sh"
