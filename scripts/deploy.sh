f726647ec8068690eb5bf07ec756c4a1f46d3918
Ben Navetta <ben.navetta@gmail.com>
2017-04-01 01:21:22 -0400

    Switch to cleaner update model

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,5 +2,6 @@
 
 BUSUN_JAR="busun/build/libs/busun-$TRAVIS_BUILD_NUMBER.jar"
 
-gcloud compute copy-files $BUSUN_JAR brownmun:/srv/busun/busun-$TRAVIS_BUILD_NUMBER.jar
-gcloud compute ssh brownmun --command="/srv/busun/activate_version.sh $TRAVIS_BUILD_NUMBER"
+gcloud compute ssh brownmun --command="/srv/busun/predeploy.sh"
+gcloud compute copy-files $BUSUN_JAR brownmun:/srv/busun/busun.jar
+gcloud compute ssh brownmun --command="/srv/busun/redeploy.sh"
