a19b7b73d9dfb736428294dbc1b06b228fc0020f
Ben Navetta <ben.navetta@gmail.com>
2017-04-05 23:47:30 -0400

    font and favicon changes

--- core/src/main/java/org/brownmun/model/Committee.java
+++ core/src/main/java/org/brownmun/model/Committee.java
@@ -1,5 +1,6 @@
 package org.brownmun.model;
 
+import com.google.common.base.Strings;
 import com.google.common.collect.ComparisonChain;
 import lombok.AccessLevel;
 import lombok.Data;
@@ -20,58 +21,65 @@ import javax.validation.constraints.NotNull;
 @EqualsAndHashCode(exclude = {"positions"})
 public class Committee implements Comparable<Committee>
 {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-	@NotBlank
-	private String name;
+    @NotBlank
+    private String name;
 
-	@NotBlank
-	private String description;
+    @NotBlank
+    private String description;
 
-	@NotBlank
-	private String shortName;
+    @NotBlank
+    private String shortName;
 
-	private boolean jointCrisis;
+    private boolean jointCrisis;
 
-	private String topic1;
+    private String topic1;
 
-	private String topic2;
+    private String topic2;
 
-	private String topic3;
+    private String topic3;
 
-	private String topic4;
+    private String topic4;
 
-	@NotNull
-	@Enumerated(EnumType.STRING)
-	private CommitteeType committeeType;
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private CommitteeType committeeType;
 
-	@OrderBy("name ASC")
-	@OneToMany(mappedBy = "committee", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-	private List<Position> positions;
+    @OrderBy("name ASC")
+    @OneToMany(mappedBy = "committee", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Position> positions;
 
-	@Setter(AccessLevel.NONE)
-	@Formula("(SELECT COUNT(*) FROM position p WHERE p.committee_id = id AND p.delegate_id IS NOT NULL)")
-	private int assignedPositions;
+    @Setter(AccessLevel.NONE)
+    @Formula("(SELECT COUNT(*) FROM position p WHERE p.committee_id = id AND p.delegate_id IS NOT NULL)")
+    private int assignedPositions;
 
-	@Setter(AccessLevel.NONE)
-	@Formula("(SELECT COUNT(*) FROM position p WHERE p.committee_id = id)")
-	private int totalPositions;
+    @Setter(AccessLevel.NONE)
+    @Formula("(SELECT COUNT(*) FROM position p WHERE p.committee_id = id)")
+    private int totalPositions;
 
-	@Transient
-	public String getContactEmail()
-	{
-		// TODO: this will break for bucs
-		return getShortName().toLowerCase() + "@busun.org";
-	}
+    @Transient
+    public String getContactEmail()
+    {
+        // TODO: this will break for bucs
+        return getShortName().toLowerCase() + "@busun.org";
+    }
 
-	@Override
-	public int compareTo(Committee o)
-	{
-		return ComparisonChain.start()
-			.compare(this.name, o.name)
-			.compare(this.id, o.id)
-			.result();
-	}
+    @Transient
+    public boolean hasTopics()
+    {
+        return !Strings.isNullOrEmpty(topic1) || !Strings.isNullOrEmpty(topic2)
+            || !Strings.isNullOrEmpty(topic3) || !Strings.isNullOrEmpty(topic4);
+    }
+
+    @Override
+    public int compareTo(Committee o)
+    {
+        return ComparisonChain.start()
+            .compare(this.name, o.name)
+            .compare(this.id, o.id)
+            .result();
+    }
 }
