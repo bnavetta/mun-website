3cfea15d1bb400a1a007e8e337263ff24ce3fe51
Ben Navetta <ben.navetta@gmail.com>
2017-04-08 12:45:21 -0400

    sort committees

--- core/src/main/java/org/brownmun/web/general/CommitteeListController.java
+++ core/src/main/java/org/brownmun/web/general/CommitteeListController.java
@@ -1,6 +1,9 @@
 package org.brownmun.web.general;
 
+import com.google.common.collect.Lists;
+import org.brownmun.model.committee.Committee;
 import org.brownmun.model.committee.CommitteeType;
+import org.brownmun.model.committee.JointCrisis;
 import org.brownmun.model.repo.CommitteeRepository;
 import org.brownmun.model.repo.JointCrisisRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +11,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 @Controller
 @RequestMapping("/conference/committees")
@@ -26,10 +34,21 @@ public class CommitteeListController
     @GetMapping
     public String committees(Model model)
     {
-        model.addAttribute("generalCommittees", repo.findAllByCommitteeType(CommitteeType.GENERAL));
-        model.addAttribute("specializedCommittees", repo.findAllByCommitteeType(CommitteeType.SPECIALIZED));
-        model.addAttribute("jointCrises", jointCrisisRepo.findAll());
-        model.addAttribute("crisisCommittees", repo.findNonJointCrisisCommittees());
+        List<Committee> generalCommittes = Lists.newArrayList(repo.findAllByCommitteeType(CommitteeType.GENERAL));
+        Collections.sort(generalCommittes, Comparator.comparing(Committee::getName));
+        model.addAttribute("generalCommittees", generalCommittes);
+
+        List<Committee> specializedCommittees = Lists.newArrayList(repo.findAllByCommitteeType(CommitteeType.SPECIALIZED));
+        Collections.sort(specializedCommittees, Comparator.comparing(Committee::getName));
+        model.addAttribute("specializedCommittees", specializedCommittees);
+
+        List<JointCrisis> jointCrises = Lists.newArrayList(jointCrisisRepo.findAll());
+        Collections.sort(jointCrises, Comparator.comparing(JointCrisis::getName));
+        model.addAttribute("jointCrises", jointCrises);
+
+        List<Committee> crisisCommittees = Lists.newArrayList(repo.findNonJointCrisisCommittees());
+        Collections.sort(crisisCommittees, Comparator.comparing(Committee::getName));
+        model.addAttribute("crisisCommittees", crisisCommittees);
         return "conference/committees";
     }
 }
