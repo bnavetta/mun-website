77b78329832a9aa433e0ec5e3f82375a83b84366
Ben Navetta <ben.navetta@gmail.com>
2017-04-21 01:29:23 -0400

    make info page null-safe

--- scripts/advisor_import/import_advisors.py
+++ scripts/advisor_import/import_advisors.py
@@ -9,7 +9,7 @@ import requests
 
 class PasswordGenerator(object):
     def __init__(self):
-        with open('/usr/share/dict/words') as f:
+        with open('words') as f:
             self.words = [word.strip() for word in f]
 
     def generate(self):
@@ -30,13 +30,14 @@ def read_advisors(filename):
             yield dict(school_name=row[0], advisor_name=row[1], advisor_email=row[2])
 
 def insert_school(cur, advisor):
-    cur.execute('INSERT INTO school (name) VALUES (%s) RETURNING id;', advisor['school_name'])
+    cur.execute('INSERT INTO school (name) VALUES (%s) RETURNING id;', (advisor['school_name'],))
     return cur.fetchone()[0]
 
 def insert_advisor(cur, advisor, school_id):
     password = pw_generator.generate()
-    cur.execute('INSERT INTO advisor (name, email, is_primary, password, school_id) VALUES (%s, %s, true, %s, %s)',
-        advisor['advisor_name'], advisor['advisor_email'], bcrypt.hashpw(password), school_id
+    password_hash = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt(10, prefix=b'2a')).decode('utf-8')
+    cur.execute("INSERT INTO advisor (name, email, is_primary, phone_number, password, school_id) VALUES (%s, %s, true, '', %s, %s)",
+        (advisor['advisor_name'], advisor['advisor_email'], password_hash, school_id)
     )
     return password
 
@@ -51,7 +52,7 @@ def send_emails(api_key, email_vars):
         'recipient-variables': json.dumps({ entry['advisor_email']: entry for entry in email_vars }),
         'h:Reply-To': 'technology@busun.org'
     }
-    resp = requests.post('https://api.mailgun.net/v3/busun.org/messages', auth=('api', api_key), data=params)
+    resp = requests.post('https://api.mailgun.net/v3/mg.busun.org/messages', auth=('api', api_key), data=params)
     print(resp.text)
     resp.raise_for_status()
 
@@ -61,8 +62,12 @@ if __name__ == '__main__':
         with conn.cursor() as cursor:
             for advisor in read_advisors('accepted.csv'):
                 school_id = insert_school(cursor, advisor)
+                print('Created school "{}" with ID {}'.format(advisor['school_name'], school_id))
                 password = insert_advisor(cursor, advisor, school_id)
+                print('Created advisor {} with password "{}" for {}'.format(advisor['advisor_email'], password, school_id))
                 params = dict(advisor)
                 params['password'] = password
                 email_vars.append(params)
+    print('Emailing advisors...')
     send_emails(os.environ['MAILGUN_API_KEY'], email_vars)
+    print('Done!')
