3665ba401414a4ce1d946a2da4b28747653344e2
Ben Navetta <ben.navetta@gmail.com>
2018-02-14 12:45:35 -0500

    set up doiuse, add breakpoint helper code, fix header size

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,34 @@
+const chalk = require('chalk');
+
 module.exports = {
     plugins: [
         require('postcss-cssnext')({
             features: {
-                customProperties: {
-                    preserve: true,
-                }
+                // Rely on browser support
+                customProperties: false
+                // customProperties: {
+                //     preserve: true,
+                // }
             }
         }),
         require('postcss-font-family-system-ui'),
+        require('doiuse')({
+            browsers: [
+                "last 2 versions",
+                "> 1%",
+                "Firefox ESR",
+                "not Baidu >= 0",
+                "not ie >= 0",
+                "not ie_mob >= 0",
+                "not OperaMobile >= 0",
+                "not OperaMini >= 0",
+                "not UCAndroid >= 0",
+                "not Edge < 16",
+                "not BlackBerry >= 0"
+            ],
+            onFeatureUsage(usageInfo) {
+                console.log(chalk.red('⚠️  ' + usageInfo.message));
+            }
+        })
     ]
 };
