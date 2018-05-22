d90698c324bb6e8ef8f781fbde9e66688a185c94
Ben Navetta <ben.navetta@gmail.com>
2018-05-22 16:53:58 -0400

    cli stuff

---
+++ cli/src/main/java/org/brownmun/cli/commands/SchoolCommands.java
@@ -0,0 +1,39 @@
+package org.brownmun.cli.commands;
+
+import org.brownmun.core.school.SchoolService;
+import org.brownmun.core.school.model.School;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.shell.standard.ShellComponent;
+import org.springframework.shell.standard.ShellMethod;
+
+import java.util.stream.Collectors;
+
+@ShellComponent
+public class SchoolCommands
+{
+    private final SchoolService schoolService;
+
+    @Autowired
+    public SchoolCommands(SchoolService schoolService)
+    {
+        this.schoolService = schoolService;
+    }
+
+    @ShellMethod("Register a new school")
+    public long registerSchool(String name, String advisorName, String advisorPassword, String advisorEmail)
+    {
+        // TODO: don't make my cell phone the placeholder
+        School school = schoolService.registerSchool(name, advisorName, advisorEmail, advisorPassword, "978-302-3343");
+        schoolService.submitApplication(school);
+        return school.getId();
+    }
+
+    @ShellMethod("Find schools by name")
+    public Iterable<School> findSchools(String nameQuery)
+    {
+        String queryLower = nameQuery.toLowerCase();
+        return schoolService.listSchools().stream()
+                .filter(s -> s.getName().toLowerCase().contains(queryLower))
+                .collect(Collectors.toList());
+    }
+}
